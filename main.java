5d56b6f03f77b633a94e4f8021157070ddf547f4
Hemant Garg <hanygarg1452@gmail.com>
2024-07-30 01:54:55 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Devs!");
+        System.out.println("I'm starting a new chapter named JENKINS");
+        System.out.println(new Date());
+    }
+
+}
